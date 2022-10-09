eaa04a201d6fefe3b134dc3afb579f35dce6a663
Ali <muhammad.ali.syaifudin@hotmail.com>
2022-10-10 00:30:45 +0700

    val or null

--- src/server/trpc/router/sudoku.ts
+++ src/server/trpc/router/sudoku.ts
@@ -4,7 +4,7 @@ import sudoku from "sudoku";
 export const sudokuRouter = t.router({
   makepuzzle: t.procedure.query(() => {
     let board = sudoku.makepuzzle();
-    board = board.map((val) => (val !== null ? val + 1 : null));
+    board = board.map((val: number | null) => (val !== null ? val + 1 : null));
     return board;
-  })
+  }),
 });
